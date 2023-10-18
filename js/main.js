934135366555921d6387c34cd921b7d8f2c453f0
aref13822003 <147720815+aref13822003@users.noreply.github.com>
2023-10-18 07:16:19 +0330

    about and home section
    create about and home section and adding java to icons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
-let navEL=document.querySelector('.navbar');
+// icon elements
+let navEL=document.querySelector('.header .navbar');
 
 const navBtn=document.querySelector('#menu-btn')
+
+const shopBasket=document.querySelector('.header .cart-items-container')
+
+const cartBtn=document.querySelector('#cart-btn')
+
+const searchBox=document.querySelector('.header .search-form ')
+const searchBtn=document.querySelector('#search-btn')
+
+
+// action icons
+navBtn.addEventListener('click',()=>{
+    navEL.classList.toggle('active')
+    searchBox.classList.remove('actives')
+    shopBasket.classList.remove('active')
+   
+})
+
+
+cartBtn.addEventListener('click',()=>{
+    shopBasket.classList.toggle('active')
+    searchBox.classList.remove('actives')
+    navEL.classList.remove('active')    
+})
+
+searchBtn.addEventListener('click',()=>{
+    searchBox.classList.toggle('actives')
+    shopBasket.classList.remove('active')
+    navEL.classList.remove('active')
+   
+})
+window.addEventListener('scroll',()=>{
+    searchBox.classList.remove('actives')
+    shopBasket.classList.remove('active')
+    navEL.classList.remove('active')
+})
+
+
+
+
+
+// 
+
+
+// slider
+
+
+
+const homeSection=document.querySelectorAll('content')
+
+
+
+console.log(homeSection)
