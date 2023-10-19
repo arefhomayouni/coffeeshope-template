7b9a8e7da05a90083b0b27f02e32b21a1b1f3d34
aref13822003 <147720815+aref13822003@users.noreply.github.com>
2023-10-19 05:50:22 +0330

    end
    all java and styles are added now

--- js/main.js
+++ js/main.js
@@ -45,36 +45,102 @@ window.addEventListener('scroll',()=>{
 // 
 
 
-// slider
+// slider and interval
 
 
 
-const homeSection=document.querySelectorAll('content')
+const preBtn=document.querySelector('.pre')
+const nextBtn=document.querySelector('.next')
+const imgEl=document.querySelectorAll('.slider img ')
+ 
+let activeSlice=0
 
+const sliderFunc=()=>{
+imgEl[activeSlice].classList.remove('swipe')
+activeSlice++
 
+if(activeSlice===imgEl.length)activeSlice=0
+imgEl[activeSlice].classList.add('swipe')
 
-console.log(homeSection)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(sliderFunc,4000)
 
 
 
 
-// 
+// show slider
+
+const sliderBtn=document.querySelector('.show-slider')
+const sliderBody=document.querySelector('.slider')
+sliderBtn.addEventListener('click',()=>{
+    sliderBody.classList.toggle('.active-slider')
+})
 
 
-fetch('https://api.sampleapis.com/coffee/hot')
+
+
+
+
+
+
+
+
+// // 
+
+
+// fetch('https://api.sampleapis.com/coffee/hot')
+
+
+
+
+
+const cartSpace=document.querySelector('#fetch-cart')
+
+
+
+
+fetch('https://fakestoreapi.com/products/')
 .then(res=>res.json())
-.then(json=>console.log(json))
+.then(json=>{
+json.map((e)=>{
+    cartSpace.innerHTML+=` 
+    
+    <div class="cart-item">
+    <span class="fas fa-times">${e.title}</span>
 
+    <img src=${e.image} alt="">
+
+
+    <div class="content">
+        <h3> cart item ${e}</h3>
+
+        <div class="price">${e.price}/-</div>
+
+    </div>
+
+</div>
+    
+    
+    
+    
+  
+    
+    `
+})
+})
+
+
+
+
+// show nav
+
+const navBody=document.querySelector('.header')
+
+
+function showNav(){
+    navBody.classList.add('show-nav')
+}
+setTimeout(showNav
+    
+, 1000);
